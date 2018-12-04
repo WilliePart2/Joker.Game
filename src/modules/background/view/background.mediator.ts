a6c18792700143e48ebb107cc816c3da63feb2b5
WilliePart2 <anarhistishche@gmail.com>
2018-12-05 01:15:34 +0200

    created base structure for mediator and ui

--- src/modules/background/view/background.mediator.ts
+++ src/modules/background/view/background.mediator.ts
@@ -1,7 +1,17 @@
 import { GameMediator } from "../../../game.core/game.classes/game.mediator";
+import { BackgroundTestUI } from "./background.test.ui";
+import { TEST } from "./ui.names";
+import { handler } from "../../../game.core/game.decorators/handler";
+import { TestUI } from "../background.notifications";
+import { Notification } from "../../../../PureMVCMulticore/core/pureMVC/notification/Notification";
 
 export class BackgroundMediator extends GameMediator {
     onRegister () {
-        
+        this.registerUI(TEST, BackgroundTestUI)
+    }
+
+    @handler(TestUI)
+    createdUI (notification: Notification<any>) {
+        console.log(notification.name, notification.body);
     }
 }
--- src/modules/background/view/background.test.ui.ts
+++ src/modules/background/view/background.test.ui.ts
@@ -1,7 +1,7 @@
 import { UIComponent } from "../../../game.core/game.classes/ui.component";
 import { TestUI } from "../background.notifications";
 
-export class BackgroundMainUI extends UIComponent {
+export class BackgroundTestUI extends UIComponent {
     prepareUI () {
         this.sendNotificationToMediator(TestUI, 'and test notifications');
     }
